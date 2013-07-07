cc92b79a2495ff7b1bbfdbbd9028b60c4e6880b9
Le Nhat Anh <lenhatanh86@gmail.com>
2013-07-07 16:47:37 +0700

    All (default settings) convert

---
+++ all.php
@@ -0,0 +1,90 @@
+<?php
+
+require 'function.php';
+
+$jsonDataDir = '/WebServer/CLDR/json/main';
+$phpDataDir = 'Data/php';
+$fileMode = 0666;
+$locale = array(
+    'ar',
+    'bg',
+    'bn',
+    'ca',
+    'cs',
+    'da',
+    'de',
+    'el',
+    'en',
+    'en-AU',
+    'en-CA',
+    'en-GB',
+    'en-HK',
+    'en-IN',
+    'es',
+    'fi',
+    'fr',
+    'he',
+    'hi',
+    'hr',
+    'hu',
+    'it',
+    'ja',
+    'ko',
+    'ml',
+    'nb',
+    'nl',
+    'pl',
+    'pt',
+    'pt-PT',
+    'ro',
+    'ru',
+    'sk',
+    'sl',
+    'sr',
+    'sv',
+    'th',
+    'uk',
+    'vi',
+    'zh',
+    'zh-Hant'
+);
+$field = array(
+    'ca-buddhist',
+    'ca-chinese',
+    'ca-coptic',
+    'ca-dangi',
+    'ca-ethiopic-amete-alem',
+    'ca-ethiopic',
+    'ca-generic',
+    'ca-gregorian',
+    'ca-hebrew',
+    'ca-indian',
+    'ca-islamic-civil',
+    'ca-islamic',
+    'ca-japanese',
+    'ca-persian',
+    'ca-roc',
+    'characters',
+    'contextTransforms',
+    'currencies',
+    'dateFields',
+    'delimiters',
+    'languages',
+    'layout',
+    'listPatterns',
+    'localeDisplayNames',
+    'measurementSystemNames',
+    'numbers',
+    'posix',
+    'scripts',
+    'territories',
+    'timeZoneNames',
+    'transformNames',
+    'units',
+    'variants'
+);
+$skiplocaleindex = false;
+
+define('IN', true);
+
+require 'convert.php';
